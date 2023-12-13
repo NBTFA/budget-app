9d8e054d5877b4754ee99995848aa2025f1f71bb
NBTFA <xiaoleiyu_19990209@163.com>
2023-12-13 13:59:09 +0800

    1

--- budget-frontend/budget_user/src/main.js
+++ budget-frontend/budget_user/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import 'font-awesome/css/font-awesome.min.css'
 import axios from 'axios';
 import routes from './router/index.js'
+import store from './store/index.js'
 
 Vue.config.productionTip = false
 
@@ -15,5 +16,6 @@ Vue.prototype.$http = axios;
 
 new Vue({
   render: h => h(App),
-  router: routes
+  router: routes,
+  store
 }).$mount('#app')
